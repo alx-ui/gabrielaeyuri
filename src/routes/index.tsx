dc9c7bb7b737cd1afd02e4d354c3ac7bcf69ce0d
ialexanderbrito <xanderzinho26@gmail.com>
2026-09-18 22:17:48 -0300

    chore: remove unused legacy pages and assets

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,20 +2,13 @@ import { Route, Routes } from 'react-router';
 
 import { FinalFeliz } from 'pages/FinalFeliz';
 import { Homepage } from 'pages/Home';
-import { Links } from 'pages/Links';
 import { NotFound } from 'pages/NotFound';
-import { NuBank } from 'pages/NuBank';
-import { Rotas } from 'pages/Routes';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/finalfeliz" element={<FinalFeliz />} />
-      <Route path="/presentes" element={<FinalFeliz />} />
-      <Route path="/pix" element={<NuBank />} />
-      <Route path="/links" element={<Links />} />
-      <Route path="/rotas" element={<Rotas />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
